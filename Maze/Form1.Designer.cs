208b494058538799339c71979fcebcd96f496a51
Deng Yu <jardy@vip.qq.com>
2020-03-14 20:32:39 +0800

    添加项目文件。

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.win = new System.Windows.Forms.PictureBox();
+            this.rat = new System.Windows.Forms.PictureBox();
+            this.Map = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.win)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblTime.ForeColor = System.Drawing.Color.Red;
+            this.lblTime.Location = new System.Drawing.Point(333, 411);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(128, 16);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "时间剩余：60秒";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(25, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "编辑地图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // win
+            // 
+            this.win.Image = ((System.Drawing.Image)(resources.GetObject("win.Image")));
+            this.win.Location = new System.Drawing.Point(194, 111);
+            this.win.Name = "win";
+            this.win.Size = new System.Drawing.Size(138, 174);
+            this.win.TabIndex = 4;
+            this.win.TabStop = false;
+            this.win.Visible = false;
+            // 
+            // rat
+            // 
+            this.rat.Image = ((System.Drawing.Image)(resources.GetObject("rat.Image")));
+            this.rat.Location = new System.Drawing.Point(441, 317);
+            this.rat.Name = "rat";
+            this.rat.Size = new System.Drawing.Size(20, 20);
+            this.rat.TabIndex = 2;
+            this.rat.TabStop = false;
+            // 
+            // Map
+            // 
+            this.Map.Image = ((System.Drawing.Image)(resources.GetObject("Map.Image")));
+            this.Map.Location = new System.Drawing.Point(-1, 0);
+            this.Map.Name = "Map";
+            this.Map.Size = new System.Drawing.Size(515, 383);
+            this.Map.TabIndex = 0;
+            this.Map.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 452);
+            this.Controls.Add(this.win);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rat);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.Map);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "老鼠走迷宫  0906401-23 邓宇";
+            ((System.ComponentModel.ISupportInitialize)(this.win)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Map)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Map;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.PictureBox rat;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox win;
+
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Maze.Properties;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i=60;
+        Color[] mycolorleft = new Color[19];
+        Color[] mycolorup = new Color[19];
+        Color[] mycolorright = new Color[19];
+        Color[] mycolordown = new Color[19];
+
+        public bool UP()//建立一个类用于判断是否是墙
+        {
+            Bitmap map = new Bitmap(Map.Image);
+            for (int i = 0; i < 19; i++)
+            {
+                mycolorup[i] = map.GetPixel(rat.Left + i, rat.Top - 13);
+            }
+            for (int a = 0; a < 19; a++)
+            {
+                if (mycolorup[a].ToArgb() != Color.White.ToArgb())//用循环判断是否有任意一个点不为通道；                                                             
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool down()
+        {
+            Bitmap bitmap = new Bitmap(Map.Image);
+            for (int i = 0; i < 19; i++)
+            { 
+                mycolordown[i] = bitmap.GetPixel(rat.Left + i, rat.Top + 9); 
+            }
+            for (int a = 0; a < 19; a++)
+            {
+                if (mycolordown[a].ToArgb() != Color.White.ToArgb())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool left()
+        {
+            Bitmap bitmap = new Bitmap(Map.Image);
+            for (int i = 0; i < 19; i++)
+            {
+                mycolorleft[i] = bitmap.GetPixel(rat.Left -12, rat.Top + i);
+            }
+            for (int a = 0; a < 19; a++)
+            {
+                if (mycolorleft[a].ToArgb() != Color.White.ToArgb())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool right()
+        {
+            Bitmap bitmap = new Bitmap(Map.Image);
+            for (int i = 0; i < 19; i++)
+            {
+                mycolorright[i] = bitmap.GetPixel(rat.Left + 10, rat.Top + i);
+            }
+            for (int a = 0; a < 19; a++)
+            {
+                if (mycolorright[a].ToArgb() != Color.White.ToArgb())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i--;
+            lblTime.Text = "时间剩余：" + i + "秒";           
+            if (i == 0)
+            {
+                timer1.Stop();
+                 DialogResult result=MessageBox.Show("时间已到，您没能在规定的时间内到达终点！", "游戏结束", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                 if (result == DialogResult.Cancel)
+                 {
+                     Application.Exit();
+                 }
+                 else 
+                 {
+                     i = 60;
+                     Map.Image = Resources.mapMaze;
+                     timer1.Start();
+                     rat.Left = 441;
+                     rat.Top = 317;
+                 }
+            }
+        }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == Keys.Up)
+            {
+                if (UP())
+                {
+                    rat.Top -= 2;
+                }
+            }
+            if (keyData == Keys.Down)
+            {
+                if (down())
+                {
+                    rat.Top += 2;
+                }
+            }
+            if (keyData == Keys.Left)
+            {
+                if (rat.Left <= 20)//当老鼠到达终点时，检测它离右边的距离；
+                {
+
+                    win.Visible = true;
+                    timer1.Stop();
+                    rat.Enabled = false;
+                    DialogResult result=MessageBox.Show("恭喜你，闯关成功","你赢了",MessageBoxButtons.RetryCancel,MessageBoxIcon.Information);
+                    if (result == DialogResult.Retry)
+                    {
+                        i = 60;
+                        rat.Enabled = true;
+                        timer1.Start();
+                        win.Visible = false;
+                        rat.Left = 441;
+                        rat.Top = 317;
+                    }
+                    else
+                    {
+                        Application.Exit();
+                    }
+
+                }
+                if (left() && rat.Left >= 0)
+                {
+                    rat.Left -= 2;
+                }
+            }
+            if (keyData == Keys.Right)
+            {
+                if (right() && rat.Left <= 440)
+                {
+                    rat.Left += 2;
+                }
+            }
+            return base.ProcessDialogKey(keyData);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 60;
+            timer1.Start();
+            rat.Left = 441;
+            rat.Top = 317;
+            Map.Image = Resources.mapMaze2;            
+        }
+    }
+}
